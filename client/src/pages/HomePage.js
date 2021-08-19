4cada329f533cd2ae02d6c236262ed831949ef17
Chitsanupong <first30952@gmail.com>
2021-08-19 20:46:17 +0700

    fix: correct wording

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,7 +4,7 @@ import styles from './HomePage.module.css';
 const HomePage = () => {
   return (
     <React.Fragment>
-      <h1 className={styles.header}>Welcome to Queueing Covid-19 Test</h1>
+      <h1 className={styles.header}>Welcome to Queueing COVID-19 Test</h1>
     </React.Fragment>
   );
 };
